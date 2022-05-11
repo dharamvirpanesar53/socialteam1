43e7494f47cea4c3810543f0b02a56c5495551da
dharamvirpanesar53 <dharamvirpanesar@gmail.com>
2022-05-11 22:31:46 +0800

    dharamvir

--- socialawareness/settings.py
+++ socialawareness/settings.py
@@ -122,9 +122,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'socialawareness/static')
+
+STATIC_URL = '/static/'
+
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'assests')
 # Default primary key field type
