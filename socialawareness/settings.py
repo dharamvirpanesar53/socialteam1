d0441269c8fffe1677f8a5397f5399b9128367fd
dharamvirpanesar53 <dharamvirpanesar@gmail.com>
2022-05-12 23:13:35 +0800

    setting

--- socialawareness/settings.py
+++ socialawareness/settings.py
@@ -139,4 +139,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 django_heroku.settings(locals())
